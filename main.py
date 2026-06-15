0ace1d27dbc8d6c2542255c1cf4a32a92900091d
quonkelley <shaquonkelley@gmail.com>
2026-06-15 14:58:49 -0400

    First test created.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def main():
-    print("Bootcamp Environment checker")
+def get_app_name() -> str:
+    return "Bootcamp Environment Checker"
+
+def main() -> None:
+    print(get_app_name())
 
 if __name__ == "__main__":
     main()
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from main import get_app_name
+
+def test_get_app_name() -> None:
+    assert get_app_name() == "Bootcamp Environment Checker"
+
